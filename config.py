006094b69e24c14b0a79622d930bec27b2ca807d
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-27 16:44:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,23 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = ("21027612") #⚠️ Required
-    API_HASH  = ("b36c5dc986f77eedd4bbf356b65eab19") #⚠️ Required
-    BOT_TOKEN = ("6726060216:AAFAoOYkSm39p1ug-D6d1REPFZm5MycogEs") #⚠️ Required
-    BOT_USERNAME = ("RokuFileRenamerRobot") #⚠️ Required
+    API_ID    = ("21027612")
+    API_HASH  = ("b36c5dc986f77eedd4bbf356b65eab19")
+    BOT_TOKEN = ("6726060216:AAFAoOYkSm39p1ug-D6d1REPFZm5MycogEs")
+    BOT_USERNAME = ("RokuFileRenamerRobot")
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","Snow_User_Data")     
-    DB_URL  = os.environ.get("mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority","") #⚠️ Required
+    DB_URL  = os.environ.get("mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority","")
  
     # other configs
     DOWNLOAD_LOCATION = "./MEGA_DOWNLOADS"
     TG_MAX_SIZE = 2040108421
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/3b7050b8b5d918a00b490.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5098097249').split()] #⚠️ Required
-    FORCE_SUB   = ("Rokubotz") #⚠️ Required Username without @
-    LOG_CHANNEL = int("-1002048442251") #⚠️ Required
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5098097249').split()]
+    FORCE_SUB   = ("Rokubotz") 
+    LOG_CHANNEL = int("-1002048442251")
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 
